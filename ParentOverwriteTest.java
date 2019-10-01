f24b528e67e678b00931edb826e022e82dd4a275
Saru22 <55977801+Saru22@users.noreply.github.com>
2019-10-01 17:01:18 +0530

    polymorphism

---
+++ ParentOverwriteTest.java
@@ -0,0 +1,13 @@
+package polyTest;
+
+public class ParentOverwriteTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static void add() {
+		System.out.println("Parent add");
+	}
+
+}
