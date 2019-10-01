cd01663ce4dfca656e51d6e890ac798bc8ba12a2
Saru22 <55977801+Saru22@users.noreply.github.com>
2019-10-01 17:07:09 +0530

    Abstract

---
+++ RBITest.java
@@ -0,0 +1,18 @@
+package abstractTest;
+
+public abstract class RBITest {
+	
+	public abstract void credit();//looks like interface 
+
+	public abstract void debit();//looks like interface no body only the description 
+
+	public static void loan() {//looks like inheritance
+
+		System.out.println("world bank loan");
+
+	}
+	/*
+	 * Its a combination of both inheritance n interface
+	 */
+
+}
